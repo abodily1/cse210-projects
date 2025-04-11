844f65f695bdbf9fe68872297be6220f139890b9
abodily1 <abrielle.bodily@gmail.com>
2025-04-10 20:00:57 -0600

    week06 learning activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square s1 = new Square("Teal", 9);
+        shapes.Add(s1);
+        Console.WriteLine($"Area: {s1.GetArea()}");
+        Console.WriteLine($"Color: {s1.GetColor()}");
+        Console.WriteLine();
+        
+        Rectangle r1 = new Rectangle("Forest Green", 5, 9);
+        shapes.Add(r1);
+        Console.WriteLine($"Area: {r1.GetArea()}");
+        Console.WriteLine($"Color: {r1.GetColor()}");
+        Console.WriteLine();
+
+        Circle c1 = new Circle("Gold", 2);
+        shapes.Add(c1);
+        Console.WriteLine($"Area: {c1.GetArea()}");
+        Console.WriteLine($"Color: {c1.GetColor()}");
+        Console.WriteLine();
     }
 }
